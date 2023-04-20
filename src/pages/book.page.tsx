2ab7682982001bc092df2f91b2c74bd2e2d73534
Edwin Gutierrez <yosael.gutierrez@gmail.com>
2023-04-20 15:22:40 -0600

    Add Pages templates

---
+++ src/pages/book.page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BookPage() {
+  return <div>BookPage</div>;
+}
---
+++ src/pages/checkoutAdmin.page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CheckoutAdminPage() {
+  return <div>CheckoutAdminPage</div>;
+}
