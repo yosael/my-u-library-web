53b61337962d3fa9c23862f3c16b5cfbbd118d77
Edwin Gutierrez <yosael.gutierrez@gmail.com>
2023-04-20 15:46:12 -0600

    Add mui and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Container } from "@mui/material";
 import "./App.css";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>App here</h1>
+      <Container>
+        <Navbar />
+      </Container>
     </div>
   );
 }
