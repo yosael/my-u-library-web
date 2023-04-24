deb1c5de75a4e96e3e2081f9bcb2b3b2e317c639
Edwin Gutierrez <yosael.gutierrez@gmail.com>
2023-04-24 09:20:53 -0600

    clean removed file

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Container } from "@mui/material";
 import { Suspense } from "react";
 import "./App.css";
 import Loader from "./components/loader";
-import UserProvider from "./components/userProvider_";
 import AppRoutes from "./components/appRoutes";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./store/storeConfig";
